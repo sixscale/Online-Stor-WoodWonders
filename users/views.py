608b788ca45e1323944308ac6dd5d7c4b8c19eed
Никита Рябов <gfgfgf354@gmail.com>
2024-01-18 03:35:06 +0300

    Сделал авторизацию и аутентификацию. Личный кабинет пользователя.

--- users/views.py
+++ users/views.py
@@ -1,26 +1,76 @@
-from django.shortcuts import render
+from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+from users.forms import UserLoginForm, UserRegistrationForm, ProfileForm
 
 
 def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                messages.success(request, f"{username}, Вы вошли в аккаунт")
+
+                if request.POST.get('next', None):
+                    return HttpResponseRedirect(request.POST.get('next'))
+
+                return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserLoginForm()
+
     context = {
         'title': 'WW - Авторизация',
+        'form': form,
     }
     return render(request, 'users/login.html', context)
 
 
 def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            messages.success(request, f"{user.username}, Вы успешно зарегистрированы и вошли в аккаунт")
+            return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserRegistrationForm()
+
     context = {
         'title': 'WW - Регистрация',
+        'form': form,
     }
     return render(request, 'users/registration.html', context)
 
 
+@login_required
 def profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f"Профиль успешно обновлен")
+            return HttpResponseRedirect(reverse('user:profile'))
+    else:
+        form = ProfileForm(instance=request.user)
+
     context = {
         'title': 'WW - Кабинет',
+        'form': form,
     }
     return render(request, 'users/profile.html', context)
 
 
+@login_required
 def logout(request):
-    pass
+    messages.success(request, f"{request.user.username}, Вы вышли из аккаунта")
+    auth.logout(request)
+    return redirect(reverse('main:index'))
