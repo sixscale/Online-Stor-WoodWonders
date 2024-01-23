7c826cd39cf7ee7a1a72e3ff626e9624134e858c
Никита Рябов <gfgfgf354@gmail.com>
2024-01-23 14:34:59 +0300

    Доделал корзину авторизованного пользователя

--- users/views.py
+++ users/views.py
@@ -18,7 +18,8 @@ def login(request):
                 auth.login(request, user)
                 messages.success(request, f"{username}, Вы вошли в аккаунт")
 
-                if request.POST.get('next', None):
+                redirect_page = request.POST.get('next', None)
+                if redirect_page and redirect_page != reverse('user:logout'):
                     return HttpResponseRedirect(request.POST.get('next'))
 
                 return HttpResponseRedirect(reverse('main:index'))
@@ -67,6 +68,10 @@ def profile(request):
         'form': form,
     }
     return render(request, 'users/profile.html', context)
+
+
+def users_cart(request):
+    return render(request, 'users/users_cart.html')
 
 
 @login_required
